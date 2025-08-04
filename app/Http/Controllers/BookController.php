a8ca0db2ef9d2cef3f4b5f4d18de04130a5c12a1
Doğukan Alkan <dogukanalkan2804@gmail.com>
2025-08-04 15:24:02 +0300

    page_add

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -16,7 +16,7 @@ class BookController extends Controller
      */
     public function index()
     {
-        $books = Book::with('author')->orderBy('name')->get();
+        $books = Book::with('author')->orderBy('name')->paginate(6);
         return view('books.index', compact('books'));
     }
 
